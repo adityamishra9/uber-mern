f9a67c4d6c2fcab11895b58aede69c593692e9d1
Aditya Mishra <adityamishra5889@gmail.com>
2025-02-13 13:35:32 +0530

    user logout & blacklist

--- backend/routes/user.routes.js
+++ backend/routes/user.routes.js
@@ -19,4 +19,6 @@ userController.loginUser)
 
 router.get('/profile', authMiddleware.authUser ,userController.getUserProfile)
 
+router.get('/logout', authMiddleware.authUser, userController.logoutUser)
+
 module.exports = router;
